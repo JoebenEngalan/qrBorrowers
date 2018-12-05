dad804b2625433f1c8559b7ad603ee0773768a2e
tron1993 <trone1993@gmail.com>
2018-12-05 13:12:59 +0800

    commit

---
+++ platforms/android/app/src/main/assets/www/js/down.js
@@ -0,0 +1,29 @@
+document.getElementById("downloadFile").addEventListener("click", downloadFile);
+
+
+
+function downloadFile() {
+    
+    var fileTransfer = new FileTransfer();
+    
+    var uri = "generate.html";
+    var fileURL =  "///storage/emulated/0/DCIM/myFile";
+ 
+    fileTransfer.download(
+        uri, fileURL, function(entry) {
+          console.log("download complete: " + entry.toURL());
+       },
+         
+       function(error) {
+          console.log("download error source " + error.source);
+          console.log("download error target " + error.target);
+          console.log("download error code" + error.code);
+       },
+         
+       false, {
+          headers: {
+             "Authorization": "Basic dGVzdHVzZXJuYW1lOnRlc3RwYXNzd29yZA=="
+          }
+       }
+    );
+ }
---
+++ platforms/android/app/src/main/assets/www/js/scanQR.js
@@ -0,0 +1,34 @@
+var resultDiv;
+
+document.addEventListener("deviceready", init, false);
+
+function init() {
+    document.querySelector("#startScan").
+        addEventListener("touchend", startScan, false);
+	
+	//resultDiv = document.querySelector("#results");
+	
+	resultDiv = document.querySelector("#text4");
+	
+}
+
+function startScan() {
+
+	cordova.plugins.barcodeScanner.scan(
+        
+        function (result) {
+			var s = result.text.split('|');
+            result.format;
+			result.cancelled;
+
+			resultDiv.value = s;
+        }, 
+        
+
+		function (error) {
+			alert("Scanning failed: " + error);
+		}
+	);
+	
+
+}
