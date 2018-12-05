032ac1929b3d690c5557b020791a089e8d551acb
tron1993 <trone1993@gmail.com>
2018-12-06 07:55:28 +0800

    commit

--- platforms/android/app/src/main/assets/www/js/borrowerhandler.js
+++ platforms/android/app/src/main/assets/www/js/borrowerhandler.js
@@ -1,10 +1,11 @@
-var borroweraddhandler ={
-    addborrower: function (borrowerID,Lname,Fname,address,email,Mobile) {
+var borrowerhandler ={
+    addBorrower: function (borrowerID,Lname,Fname,addresses,email,Mobile) {
         databasehandler.db.transaction(
             function(tx){
-                tx.excuteSql(
-                    "Insert into Borrowers(borrowerID,Lname,Fname,Address,Email,Mobile Number) values(?,?,?,?,?,?)",
-                    [borrowerID,Lname,Fname,address,email,Mobile],
+                tx.executeSql(
+                    "Insert into borrowers(borrowerID,Lname,Fname,addresses,Email,MobileNumber)" + 
+                    "values(?,?,?,?,?,?)",
+                    [borrowerID,Lname,Fname,addresses,email,Mobile],
                     function(tx,result){},
                     function(tx,error){
                         console.log("add borrower error: "+error.message);
--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -2,5 +2,17 @@ $(document).on("ready",function(){
     databasehandler.createDatabase();
 });
 function addBorrower(){
-    
+    var borrowerID = $("#txtNumber").val();
+    var Lname = $("#txtLname").val();
+    var Fname = $("#txtFname").val();
+    var addresses = $("#txtaddress").val();
+    var email = $("#txtEmail").val();
+    var Mobile = $("#txtMobNum").val();
+    borrowerhandler.addBorrower(borrowerID,Lname,Fname,addresses,email,Mobile);
+    $("#txtNumber").val("");
+    $("#txtLname").val("");
+    $("#txtFname").val("");
+    $("#txtaddress").val("");
+    $("#txtEmail").val("");
+    $("#txtMobNum").val("");
 }
