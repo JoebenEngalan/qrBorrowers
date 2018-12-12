0682111f8697aafef48c7e309184b6c157753687
tron1993 <trone1993@gmail.com>
2018-12-12 18:20:30 +0800

    commit

--- platforms/browser/www/js/databasehandler.js
+++ platforms/browser/www/js/databasehandler.js
@@ -2,14 +2,14 @@ var databasehandler = {
     db:null,
     createDatabase: function(){
         this.db = window.openDatabase(
-            "DB_main.db",
+            "DBtest.db",
             "1.0",
             "borrow database",10000);
         this.db.transaction(
             function(tx) {
                 ////
                 tx.executeSql(
-                    " create table if not exists borrowers(_borrowerID integer primary key, Lname text, Fname text, Fname text, addresses text, Email text, MobileNumber integer) ",
+                    " create table if not exists borrowers(borrowerID integer primary key, Lname text, Fname text, Fname text, addresses text, Email text, MobileNumber integer) ",
                     [],
                     function(tx,result){},
                     function(tx,error) {
